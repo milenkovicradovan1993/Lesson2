b47f847fbb70fe305a756cd8af0a19d279ffc47e
Radovan <Radovan1993@live.com>
2019-04-18 20:57:11 +0200

    Lesson2 Change

--- src/exercises/NASACountdown.java
+++ src/exercises/NASACountdown.java
@@ -2,10 +2,19 @@ package exercises;
 
 /*
  * Count down a rocket launch!
- * 
- * Print the numbers from 10 to 1, then print “blastoff”. 
+ * . 
  */
 
 public class NASACountdown {
+	public static void main(String[] args) {
+		for (int i = 10; i >=0; i--) {
+		/*	JOptionPane.showMessageDialog(null, i);
+		}
+		JOptionPane.showMessageDialog(null, "Blastoff!!!"); */
+			System.out.println(i);
+	}
+		System.out.println("Blastoff");
 
+
+}
 }
