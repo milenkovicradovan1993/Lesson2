d4de49db9e77c6c7b42ad631dd709faef53ea233
Radovan <Radovan1993@live.com>
2019-04-21 16:23:13 +0200

    PentagonCrazy change

--- src/exercises/PentagonCrazy.java
+++ src/exercises/PentagonCrazy.java
@@ -7,29 +7,23 @@ import org.jointheleague.graphical.robot.Robot;
 public class PentagonCrazy {
 
 	private void makePrettyThings() {
-		// 1. Create a new Robot
+		Robot rob = new Robot();
 
-		// 3. Put the robot's pen down
-
-		// 8. Make the robot go at maximum speed (10)
-
-		// 9. Set the pen to a color that you like for the shape
-
-		// 4. Make a variable for the number of sides you want (can’t test this one)
-
-		// 5. Make a variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6.
-
-		// 7. Do steps #2 to #11, 200 times. When this is done you should see a pentagon.
-
-			// 2. Move the robot 200 pixels
+		rob.penDown();
+		rob.setSpeed(10);
+		rob.setPenColor(0, 5, 120);
+		int numberofSides = 4;
+		double angle = 360/5;
 	
-			// 10. Make the robot move "i" pixels instead of 200 (don’t need new line of code for this, just change previous one)
+	      for (int i = 0; i <200; i++)  {
+	    	  rob.move(i);
+	    	  rob.turn(angle+1);
+	    	  
+	    	  
+	        	
+	        }
+		}
 	
-			// 6. Turn the robot the amount in your angle variable
-	
-			// 11. Turn the robot one more degree
-
-	}
 
 	// Variations:
 	// *12. make the pattern really huge
