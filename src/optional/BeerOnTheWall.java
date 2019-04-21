65b3f57a916789377e3136a03c0e88466fe63ba5
Radovan <Radovan1993@live.com>
2019-04-21 16:54:17 +0200

    BeerOnTheWall Change

--- src/optional/BeerOnTheWall.java
+++ src/optional/BeerOnTheWall.java
@@ -9,5 +9,22 @@ package optional;
 public class BeerOnTheWall {
 	public static void main(String[] args) {
 
+		for (int i = 99; i >=3; i--) {
+			System.out.println( i + " bottles of beer on the wall, " +  i + " bottles of beer. ");
+		    System.out.println("Take one down and pass it around, " + (i-1) + " bottles of beer on the wall.");
+		    System.out.println();
+
+		    }   
+		        System.out.println("2 bottles of beer on the wall, 2 bottles of beer.");
+		        System.out.println("Take one down and pass it around, 1 bottle of beer on the wall.");
+		        System.out.println();
+		    	System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+		    	System.out.println("Take one down and pass it around, no more bottles of beer on the wall.");
+		    	System.out.println();
+		    	System.out.println("No more bottles of beer on the wall, no more bottles of beer. ");
+		    	System.out.println("Go to the store and buy some more, 99 bottles of beer on the wall.");	
+			}    
+		
+
 	}
-}
+
